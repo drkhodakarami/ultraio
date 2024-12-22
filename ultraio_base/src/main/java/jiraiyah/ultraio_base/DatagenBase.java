30007782330967ddaa80731be712de18e4af0bae
khodakarami <khodakarami_dr@outlook.com>
2024-12-22 18:49:25 +0330

    Main Mod - Initial commit UltraIO-Base - Initial commit

---
+++ ultraio_base/src/main/java/jiraiyah/ultraio_base/DatagenBase.java
@@ -0,0 +1,11 @@
+package jiraiyah.ultraio_base;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DatagenBase implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ ultraio_base/src/main/java/jiraiyah/ultraio_base/ReferenceBase.java
@@ -0,0 +1,11 @@
+package jiraiyah.ultraio_base;
+
+import jiraiyah.reference.JiReference;
+
+public class ReferenceBase extends JiReference
+{
+    public ReferenceBase(String mod_ID)
+    {
+        super(mod_ID);
+    }
+}
